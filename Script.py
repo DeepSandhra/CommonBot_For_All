cbb670be3d642f9d1ee1ced26ce6c9ba45743270
DeepSandhra <109024247+DeepSandhra@users.noreply.github.com>
2022-09-17 16:36:39 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,21 +1,19 @@
 class script(object):
-    START_TXT = """Hᴇʟʟᴏ {},
-Mʏ ɴᴀᴍᴇ 𝙸𝚂 <a href=https://t.me/{}>{}</a>, ℹ️ ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇꜱ , ᴊᴜꜱᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴇɴᴊᴏʏ ⚡"""
-    HELP_TXT = """Hᴇʏ {}
+    START_TXT = """𝙷𝙴𝙻𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/movi2x><b><i>★[Kᕼᴜsᕼ Bᴏᴛᴢ]★</i></b></a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Technomindzyt><b><i>★彡[ʀᴀᴠɪɴᴀɴᴅᴀɴ]彡★</i></b></a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-✯ Uᴘᴅᴀᴛᴇs : <a href=https://t.me/Movies_hunt_2022><b><i>Fᴏʟʟᴏᴡ ᴜs</i></b></a>
-✯ Cᕼᴀɴɴᴇʟ : <a href=https://t.me/Movies_DATABASE_2022><b><i>Fᴏʟʟᴏᴡ ᴜs</i></b></a>
     SOURCE_TXT = """<b>NOTE:</b>
-- Kᕼᴜsᕼ Bᴏᴛᴢ is A Open Source Project. 
+- Techno Mindz is A Open Source Project. 
 - You Can Find The Repo Link Below 👇👇
-- Source - <a href=https://github.com/DeepSandhra/CommonBot_For_All><b><i>Kᕼᴜsᕼ Bᴏᴛᴢ</i></b></a>  
+- Source - <a href=https://github.com/TechnoMindz/CommonBot_For_All><b><i>Techno Mindz</i></b></a>
 
 <b>DEVS:</b>
 - <a href=https://t.me/movi2x><b><i>★[Kᕼᴜsᕼ Bᴏᴛᴢ]★</i></b></a>"""
